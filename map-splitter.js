0854f4f5f1065ff4a97e86af21b1e43f0c8f04c0
YousafAzabi <joeazabi@gmail.com>
2018-09-25 12:29:27 +0100

    Working map-splitter.js. Divide map to 2^n smaller areas

--- map-splitter.js
+++ map-splitter.js
@@ -4,33 +4,35 @@ splits the map to smaller areas for parallel processing
 */
 const {execSync} = require('child_process'); //to run commandline Synchronous
 const {exec} = require('child_process'); //to run commandline Asynchronous
-const config = require('./map-config.json'); //configaration file
+const config = require('./map-splitter-config.json'); //configaration file
 const tm = require('./timeprinter'); //to print time in **h:**m:**s format
+
+console.log('\n\t*****\t Script started at ' +
+            new Date().toTimeString().slice(0,8) + ' \t*****\n');
 
 //record total time for script while running
 let totalTime = new Date();
 
 //load start and ending coordinates from config file
-let long = config.startLong, lat = config.startLat;
+let long1 = config.startLong, lat1 = config.startLat;
 let long2 = config.endLong, lat2 = config.endLat;
-
-//calculate  map size in tems of coordinates
-const longInc = (long2 - long) / config.longDivision;
-const latInc = (lat2 - lat) / config.latDivision;
 
 //output files for whole map and input for splitMap function
 const fileTag = config.fileTag;
+const outPath = config.outPath;
 const asyncTag = config.Async.toLowerCase();
+const segements = Math.pow(2, (config.stages));
 
-//number of divisions (small areas)
-const numArea = config.latDivision * config.longDivision;
+let segArray = [];
+let timer = [];
+let checkFile = new Array(segements / 2).fill(0);
 
 //define ogr2ogr parameters
 let ogrCommand = '';
 // define output format (JSON)
-let ogrJSON = 'ogr2ogr -f GeoJSON -skipfailures -dim XY';
+let ogrJSON = 'ogr2ogr -f GeoJSON -skipfailures -dim XY ';
 //define area coordinates of the map
-let ogrClip = ' -clipdst ' + long + ' ' + lat + ' ' + long2 + ' ' + lat2;
+let ogrClip = ' -clipdst ' + long1 + ' ' + lat1 + ' ' + long2 + ' ' + lat2;
 //define SQL query for OSM map to select related links
 let ogrSQLOSM = " -sql \"SELECT * FROM lines WHERE highway in " +
             "('motorway', 'trunk', 'motorway_link', 'trunk_link', " +
@@ -38,101 +40,107 @@ let ogrSQLOSM = " -sql \"SELECT * FROM lines WHERE highway in " +
             "'primary_link', 'secondary_link', 'residential', " +
             "'service', 'living_street', 'unclassified')\" ";
 //define SQL query for OS map to select related data
-let ogrSQLOS = ' -sql "SELECT localid,roadname,directionality,formofway,geom ' +
+let ogrSQLOS = ' -sql "SELECT localid, roadname, directionality, formofway, geom ' +
             'FROM roadlink " ';
 //define projection system for OS map
 let ogrProj = ' -s_srs "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 ' +
             '+x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs ' +
             '+nadgrids=./InputY/OSTN15_NTv2_OSGBtoETRS.gsb" -t_srs Epsg:4326 ';
 
-//closure to split maps to smaller areas has its paramter INPUT FILE NAME
-splitMap = (() => {
-  var stack = 0, index = 0; //this sentance only executed once.
-  return () => {
-    if( (index % config.longDivision) == 0 ) { //check to reset latitude when reached most right side
-      lat = lat2; //move latitude to next position
-      lat2 = lat + latInc;  //end of the map segement latitude
-      long = config.startLong; //reset longitude to start from most left side
-    }
-    index ++; // index for area number
-    long2 = long + longInc;  //end of the map segement longitude
-    ogrClip = ' -clipdst ' + long + ' ' + lat + ' ' + long2 + ' ' + lat2; //coordinates square
-    //let index = j + (i * config.longDivision); // to name files in order
-    let output = ' .' + fileTag.split('.')[1] + '_' + index + '.json '; // output file name
-    let timer = new Date();
-    ogrCommand = ogrJSON + output + fileTag + ogrClip;
-    commandLine(index, ogrCommand, function(i, stdout) {
-      tm.print('\t\tArea number ' + i + ' took: \t', new Date() - timer);
-      stack ++; //increment number of element in stack (area in process)
-      //console.log('Stack size is: ' + stack + ' another area is being processed')
-      if( (index < numArea) && (config.Async.toLowerCase() == 'yes')) {
-        splitMap();
-      }
-      if ( (stack == numArea) ){
-        printTotalTime();
-      }
-    }); // call function
-    long = long2; //set start longituide of next map segement
-  }
-})();
-
 //function to execute commandline
-commandLine = (i, command, callback) => {
+commandLine = (id, command, callback) => {
   if(asyncTag == 'yes') {  //execute commandline Synchronous
     exec(command, (error, stdout, stderr) => {
       if (error) { //check for error
         console.error('exec error: ' + error); // print error
       }
-      callback(i, stdout);
+      callback(id);
     });
   } else {  //execute commandline Asynchronous
     execSync(command);
-    callback(i);
+    callback(id);
   }
 }
 
-printTotalTime = () => {
-  //print total time took the script to run
-  console.log('\n\t********************************************');
-  tm.print('\t\tTotal time taken: \t', new Date() - totalTime);
-  console.log('\t********************************************\n');
+//
+splitMap = (id, arr) => {
+  timer[id] = new Date();
+  let [x1, y1, x2, y2] = arr;
+  let ogrClip = ' -clipdst ' + x1 + ' ' + y1 + ' ' + x2 + ' ' + y2;
+  let output = outPath.trim() + fileTag + id + '.json '; // output file name
+  let input = outPath.trim() + fileTag + Math.floor(id / 2) + '.json';
+  ogrCommand = ogrJSON + output + input + ogrClip;
+  segArray[id] = arr
+  console.log( id + '   ' + ogrCommand);
+  commandLine(id, ogrCommand, function(i) {
+    tm.print('\t\tArea number ' + i + ' took: \t', new Date() - timer[i]);
+    if (i < segements) {
+      setMapDimensions(i);
+    }
+    //check file to be deleted
+    let tempIndex = Math.floor(i / 2);
+    if ( (++ checkFile[tempIndex]) == 2) {
+      let file = outPath.trim() + fileTag + tempIndex + '.json';
+      exec('rm ' + file, (error,stdout,stderr) => {
+        if (error) {
+          console.error('exec error: ' + error);
+        }
+        console.log('File ' + file +  'has been deleted.');
+      });
+    }
+    if(++ checkFile[0] >= 30) {
+      tm.print('\t\tTotal time taken: \t', new Date() - totalTime);
+    }
+  }); // call function
+}
+
+//split maps to smaller areas has its paramter INPUT FILE NAME
+setMapDimensions = (id) => {
+  let mid;
+  let [x1, y1, x2, y2] = segArray[id];
+  if ( ! (Math.floor(Math.log2(id)) % 2) ) { //testing number in which 2^x group
+    mid = (y1 + y2) / 2;
+    splitMap((2 * id), [x1, y1, x2, mid]);
+    splitMap((2 * id + 1), [x1, mid, x2, y2]);
+  } else{
+    mid = (x1 + x2) / 2;
+    splitMap((2 * id), [x1, y1, mid, y2]);
+    splitMap((2 * id + 1), [mid, y1, x2, y2]);
+  }
 }
 
 //script starts here
-let tempString, threads; //variables to initalizing Async or Sync
+let tempString;
 //output the mode used Asynchronous or Synchronous
 if(asyncTag == 'yes'){ //compare mode if Asynchronous
   tempString = 'As'; //change mode message to Asynchronous
-  threads = config.threads; //number of areas to be processed
 } else {
   tempString = 'S'; //set mode message to default (Synchronous)
-  threads = numArea; //max number of area processed simultaneously
 }
 console.log('\n \t\t************************\n \t\t** ** ' + tempString +
   'ynchronous ** **\n \t\t************************\n'); //output mode message
 
 //read map source type from config JSON file
 tempString = config.map.toUpperCase();
+ogrCommand = ogrJSON + ogrClip + outPath + fileTag + '1.json';
 //comapre which map source to use
 switch(tempString){
   case 'OS': //case to prepare commandline for OS
-    ogrCommand = ogrJSON + ogrClip + fileTag + config.inputOS + ogrSQLOS + ogrProj;
+    ogrCommand =  ogrCommand + config.inputOS + ogrSQLOS + ogrProj;
     break;
   case 'OSM': //case to prepare commandline for OSM
-    ogrCommand = ogrJSON + ogrClip + fileTag  + config.inputOSM + ogrSQLOSM;
+    ogrCommand = ogrCommand + config.inputOSM + ogrSQLOSM;
     break;
   default: //default case print error message and exit
-    throw 'ERROR! Please choose map source you want to convert: OS or OSM';
+    throw 'ERROR! Map source error. In config file map should be: OS or OSM';
 }
 console.log('\n************* Script is runing for ' + tempString + ' map *************');
 
-commandLine(0, ogrCommand, function() {
-  console.log('\nThe pre-processing of main map is done.\n');  //print ID of processed segement
+commandLine(1, ogrCommand, function(id) {
+  tm.print('\nThe pre-processing of main map is done. It took: ', new Date() - totalTime);
   // output total number of divisions (areas) to console
-  console.log(' ********** Now dividing the map to ' + numArea
+  console.log(' ********** Now dividing the map to ' + segements
               + ' areas **********');
-  lat2 = lat;  //initial value for programme to run correctly
-  for(let i = 0; i < threads; i++){
-    splitMap(); //spiltMap function
-  }
+  segArray[id] = [long1, lat1, long2, lat2];
+  setMapDimensions(id); //
 });
